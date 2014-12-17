4b9260bd22bdd4833d9467267b4371cbfc265027
Colibaba Alexandr <sanea_ko@mail.ru>
2014-12-17 14:49:20 +0200

    html/css

--- index.php
+++ index.php
@@ -1,27 +1,53 @@
 <!doctype html>
 <html>
 <head>
-	<charset="utf-8" />
+	<meta charset="utf-8" />
 	<title>Promo site</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css"/>
 </head>
 <body lang="ru">
-<div>
-	<header>
-		<nav>
+<header>
+	<div class="size960">
+		<div id="logo"></div>
+		<nav id="top">
 			<menu>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+				<li>О нас</li>
+				<li>Услуги</li>
+				<li>Тарифы</li>
+				<li>Контакты</li>
 			</menu>
 		</nav>
-	</header>
-	<div>
-		<article></article>
-		<section></section>
-		<section></section>
-		<section></section>
-		<section></section>
+	</div>
+</header>
+<div id="content">
+	<div class="size960">
+		<aside>
+			<nav id="left">
+				<menu>
+					<li>О нас</li>
+					<li>Услуги</li>
+					<li>Тарифы</li>
+					<li>Контакты</li>
+				</menu>
+			</nav>
+		</aside>
+		<section>
+			<article>
+				<h2>Краткое описание нашей компании</h2>
+			</article>
+			<section id="about">
+				<iframe></iframe>
+			</section>
+			<section id="services">
+				<iframe></iframe>
+			</section>
+			<section id="tariffs">
+				<iframe></iframe>
+			</section>
+			<section id="contacts">
+				<iframe></iframe>
+			</section>
+		</section>
 	</div>
 </div>
 </body>
