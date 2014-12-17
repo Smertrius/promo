be271ae03afbc627db33c8b3ead0e80820499257
Colibaba Alexandr <sanea_ko@mail.ru>
2014-12-17 13:48:07 +0200

    template HTML

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
+<!doctype html>
 <html>
 <head>
 	<charset="utf-8" />
 	<title>Promo site</title>
 </head>
-<body>
+<body lang="ru">
+<div>
+	<header>
+		<nav>
+			<menu>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</menu>
+		</nav>
+	</header>
+	<div>
+		<article></article>
+		<section></section>
+		<section></section>
+		<section></section>
+		<section></section>
+	</div>
+</div>
 </body>
 </html>
